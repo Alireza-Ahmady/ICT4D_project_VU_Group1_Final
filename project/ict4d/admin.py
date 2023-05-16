de77201bdde15d9dcff133b823ca84bdb46400d0
Ubuntu <admin_ict4d@ICT4D-1.b0j0awcole1ebd4x34wrqudbte.fx.internal.cloudapp.net>
2023-05-16 15:36:31 +0000

    Add jupyter_notebook

--- project/ict4d/admin.py
+++ project/ict4d/admin.py
@@ -2,17 +2,12 @@ from django.contrib import admin
 from .models import Malaria_case
 from django.contrib.admin import AdminSite
 from django.contrib import admin
-from plotly.subplots import make_subplots
-import plotly.graph_objects as go
 import pandas as pd
 from .models import Malaria_case
-
 from django.contrib import admin
 from .models import Malaria_case
 from django.db.models import Count
 from django.db.models.functions import TruncMonth
-#from django_chartit import DataPool, Chart
-from chartit import Chart, PivotDataPool, PivotChart, DataPool
 from django.shortcuts import render
 
 
